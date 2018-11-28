d280a6a7aceccb6cccdf52ab79b5c89eaba3c9bc
Hannah Weissman <hweissma@wellesley.edu>
2018-11-28 22:34:51 +0000

    Hannah's python file searching/filtering donation history

---
+++ search_donation_history.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python2.7
+
+"""
+Searches for the movie by title.
+
+----------------------------------------------------------------
+CS 304 - Databases 
+Assignment 5
+October, 2018
+"""
+
+import sys
+import MySQLdb
+
+def getConn(db):
+    """A function that opens a connection with the database
+    """
+    return MySQLdb.connect(host='localhost',
+                           user='hweissma',
+                           passwd='',
+                           db=db)
+                    
+                           
+def getAllDonationHistoryInfo(conn, rowType='dictionary'):
+    """Returns all donations, in the order they were 
+    entered in the database."""
+    if rowType == "tuple":
+        curs = conn.cursor()
+    elif rowType == "dictionary":
+        # results as Dictionaries
+        curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute(
+        '''select donationID, submitDate, amount, type from donation''')
+    return curs.fetchall()
+    
+                               
+def getDonationByDonor(conn, donationID, rowType='dictionary'):
+    """Returns all donations given by a specific donor."""
+    if rowType == "tuple":
+        curs = conn.cursor()
+    elif rowType == "dictionary":
+        # results as Dictionaries
+        curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select donationID, submitDate, 
+    amount, type from donation where donationID = %s''', (donationID))
+    return curs.fetchall()
+    
+def getDonationByType(conn, type, rowType='dictionary'):
+    """Returns all donations of a specific type."""
+    if rowType == "tuple":
+        curs = conn.cursor()
+    elif rowType == "dictionary":
+        # results as Dictionaries
+        curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select donationID, submitDate,
+    amount, type from donation where type = %s''', (typ))
+    return curs.fetchall()
+
+
+
+if __name__ == '__main__':
+    conn = getConn('wmdb')
+    
