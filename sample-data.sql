08b4e6d89ca3e9823a5dc7d2b09e9a8622d2a8de
Hannah Weissman <hweissma@wellesley.edu>
2018-12-10 15:18:19 -0500

    Added sample data to populate setStatus table

--- sample-data.sql
+++ sample-data.sql
@@ -30,7 +30,48 @@ insert into inventory (description, `type`, amount, units)
 -- expenditure, 
 insert into expenditure (description, `type`, `date`, amount) 
     values 
+-- donor values
+insert into donor (name, `type`, email) 
+    values
+        ('Save the Children', 'organization', 'savethechildren@gmail.com'),
+        ('Bill Gates', 'individual', 'billgates11@gmail.com'),
+        ('The Mormon Church', 'organization', 'mormonchurchofutah@gmail.com');
+        
+
+
+-- donation values
+insert into donation (submitDate, description, amount, units,`type`, donorID) 
+    values
+        ('2018-11-07', 'Winter Sleeping Bags', 4, 'bags', 'supplies', 1),
+        ('2018-02-24', 'carrots', 4, 'bushels','food', 1),
+        ('2018-10-07', 'blankets', 20, 'pairs','supplies', 2),
+        ('2018-10-31', 'arts and crafts supplies', 4, 'bags', 'supplies', 2),
+        ('2018-11-07', 'flour', 20, 'pounds', 'food', 2),
+        ('2018-11-07', 'eggs', 48, 'cartons', 'food', 1);
+
+-- inventory
+insert into inventory (description, `type`, amount) 
+    values 
+        ('carrots', 'food', 3),
+        ('Winter sleeping bags', 'supplies', 2),
+        ('blankets', 'supplies', 12),
+        ('eggs', 'food', 24),
+        ('flour', 'food', 1);
+
+-- expenditure, 
+insert into expenditure (description, `type`, `date`, amount) 
+    values 
         ('weekly payroll', 'in house', '2018-11-05', 585),
         ('bandages', 'medical', '2018-11-06', 75),
         ('eggs', 'food', '2018-11-02', 50),
         ('blankets', 'supplies', '2018-11-06', 100);
+        
+-- setStatus
+insert into setStatus (item_id, thresholdLow, thresholdHigh)
+    values
+        (1, 5, 20),
+        (2, 10, 50),
+        (3, 5, 15),
+        (4, 6, 18)
+        (5, 1, 10);
+        
