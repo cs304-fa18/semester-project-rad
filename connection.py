b1fee07cd5eed6ccc54e296015fd205c4801ebbd
Angela Wang <awang14@wellesley.edu>
2018-12-20 08:10:47 +0000

    Extra files

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 def get_conn(db='c9'):
     c = MySQLdb.connect(host='localhost',
-                           user='arivera4',
+                           user='cotequotey',
                            passwd='',
                            db=db)
     c.autocommit(True)
